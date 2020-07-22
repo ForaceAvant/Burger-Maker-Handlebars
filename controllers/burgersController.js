4c45de8763e34cfc4c18b7df0d799c5846a47ac9
Forace Avant <themattavant@gmail.com>
2020-07-22 11:21:00 -0700

    Finished burger controller and set up main.handlebars

---
+++ controllers/burgersController.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const burger = require("../models/burger");
+
+const router = express.Router();
+
+
+//READ 
+router.get("/", function(req,res){
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req,res){
+    burger.selectAll(function(data) {
+        res.render("index", { burger_info: data});
+    });
+});
+
+//CREATE
+
+router.post("burgers/create", function(req, res) {
+    burger.createOne(req.body.burger_info, function(result){
+        res.redirect("/");
+    });
+});
+
+//UPDATE
+
+router.put("/burgers/:id", function(req, res) {
+    burger.updateOne(req.params.id, function(result) {
+        res.sendStatus(200);
+    });
+});
+
+module.exports = router;
