633f3b1f1e51494a3d4b860dd0eff6f274789008
Forace Avant <themattavant@gmail.com>
2020-07-22 11:29:01 -0700

    added boilerplate for express/handlebars servers

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgersController");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log(`Server is listening on PORT: ${PORT}`);
+})
